04054675857319d58a33cdc0db9c87dc019060f1
oj <owainj@gmail.com>
2015-02-03 21:38:07 +0700

    Updated for testing 2

--- src/Connect4.java
+++ src/Connect4.java
@@ -1,7 +1,9 @@
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.Cursor;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -9,6 +11,9 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 /**
@@ -87,11 +92,7 @@ public class Connect4 extends AnchorPane implements Connect4able {
             setOnMouseClicked(event -> {
                 int col = (int) (event.getX() / (2 * (TOKEN_RADIUS + TOKEN_SPACING)));
                 addToken(col);
-                int row = 0;
-
-                while (row < ROWS && ((Color)gameBoard[row][col].getFill()).equals(Color.WHITE))
-                    row++;
-                highlightedColumn.setHeight(row *  2 * (TOKEN_RADIUS + TOKEN_SPACING));
+                updateHighlighter(col);
 
                 if (currentPlayer.equals(Color.RED))
                     currentPlayer = Color.YELLOW;
@@ -115,12 +116,7 @@ public class Connect4 extends AnchorPane implements Connect4able {
             setOnMouseMoved(event -> {
                 int col = (int) (event.getX() / (2 * (TOKEN_RADIUS + TOKEN_SPACING)));
 
-                int row = 0;
-                while (row < ROWS && ((Color)gameBoard[row][col].getFill()).equals(Color.WHITE))
-                    row++;
-                highlightedColumn.setHeight(row *  2 * (TOKEN_RADIUS + TOKEN_SPACING));
-
-                highlightedColumn.setLayoutX(col * 2 * (TOKEN_RADIUS + TOKEN_SPACING));
+                updateHighlighter(col);
             });
 
 
@@ -149,7 +145,6 @@ public class Connect4 extends AnchorPane implements Connect4able {
             }));
             timeline.play();
         }
-
     }
 
     private void addToken(int col) {
@@ -167,6 +162,24 @@ public class Connect4 extends AnchorPane implements Connect4able {
 
             gameBoard[r][col].setFill(currentPlayer);
         }
+
+        if (getWinner() != null) {
+            Text gameOver = new Text("Game Over");
+            gameOver.setFont(Font.font("Helvetica", FontWeight.BOLD, 112));
+            gameOver.setFill(getWinner());
+            gameOver.setStroke(Color.BLACK);
+            gameOver.setStrokeWidth(3);
+            gameOver.setTextOrigin(VPos.BASELINE);
+            gameOver.setEffect(new DropShadow(4,2,2,Color.DARKGREY));
+            getChildren().add(gameOver);
+            gameOver.setX(this.getWidth() / 2 - gameOver.getBoundsInLocal().getWidth() / 2);
+            gameOver.setY(this.getHeight() / 2 - gameOver.getBoundsInLocal().getHeight() / 2);
+            setOnMouseClicked(null);
+            setOnMouseEntered(null);
+            setOnMouseExited(null);
+            setOnMouseMoved(null);
+
+        }
     }
 
     private Color[][] duplicate() {
@@ -181,6 +194,14 @@ public class Connect4 extends AnchorPane implements Connect4able {
         return a;
     }
 
+    private void updateHighlighter(int col) {
+        int row = 0;
+        while (row < ROWS && ((Color)gameBoard[row][col].getFill()).equals(Color.WHITE))
+            row++;
+        highlightedColumn.setHeight(row *  2 * (TOKEN_RADIUS + TOKEN_SPACING));
+
+        highlightedColumn.setLayoutX(col * 2 * (TOKEN_RADIUS + TOKEN_SPACING));
+    }
 
     @Override
     public void dropToken(int col) {
@@ -189,27 +210,130 @@ public class Connect4 extends AnchorPane implements Connect4able {
 
     @Override
     public void clear() {
-
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                gameBoard[r][c].setFill(Color.WHITE);
+            }
+        }
     }
 
     @Override
     public boolean hasWinner() {
-        return false;
+        return getWinner() != null;
     }
 
     @Override
     public Color getWinner() {
-        return null;
+        if (checkWinner(Color.RED))
+            return Color.RED;
+        else if (checkWinner(Color.YELLOW))
+            return Color.YELLOW;
+        else
+            return null;
+    }
+
+    private boolean checkWinner(Color color) {
+        //check horizontal
+        for (int r = 0; r < ROWS; r++) {
+            int run = 0;
+            for (int c = 0; c < COLS; c++) {
+                if ((gameBoard[r][c].getFill()).equals(color)) {
+                    run++;
+
+                    if (run == 4)
+                        return true;
+                } else {
+                    run = 0;
+                }
+            }
+        }
+
+        //check vertical
+        for (int c = 0; c < COLS; c++) {
+            int run = 0;
+            for (int r = 0; r < ROWS; r++) {
+                if ((gameBoard[r][c].getFill()).equals(color)) {
+                    run++;
+
+                    if (run == 4)
+                        return true;
+                } else {
+                    run = 0;
+                }
+            }
+        }
+
+        //check diagonal1
+        int r = 3, c = 0;
+
+        while (c < COLS - 3) {
+            int i = 0;
+            int run = 0;
+
+            while (r - i >= 0 && c + i < COLS) {
+                if ((gameBoard[r - i][c + i].getFill()).equals(color)) {
+                    run++;
+
+                    if (run == 4)
+                        return true;
+                } else {
+                    run = 0;
+                }
+                i++;
+            }
+
+            if (r < ROWS - 1) {
+                r++;
+            } else {
+                c++;
+            }
+        }
+
+        //check diagonal2
+        r = 3;
+        c = COLS - 1;
+
+        while (c > 2) {
+            int i = 0;
+            int run = 0;
+
+            while (r - i >= 0 && c - i >= 0) {
+                if ((gameBoard[r - i][c - i].getFill()).equals(color)) {
+                    run++;
+
+                    if (run == 4)
+                        return true;
+                } else {
+                    run = 0;
+                }
+                i++;
+            }
+
+            if (r < ROWS - 1) {
+                r++;
+            } else {
+                c--;
+            }
+        }
+
+        return false;
     }
 
     @Override
     public boolean isGameOver() {
-        return false;
+        return hasWinner() || isDraw();
     }
 
     @Override
     public boolean isDraw() {
-        return false;
+        if (getWinner() != null)
+            return false;
+
+        for (int i = 0; i < COLS; i++) {
+            if (gameBoard[0][i].getFill().equals(Color.WHITE))
+                return false;
+        }
+        return true;
     }
 
     @Override
