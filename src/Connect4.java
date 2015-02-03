09a4cae213a2b8a317ba5069f5dc53cfa1614c5d
oj <owainj@gmail.com>
2015-02-03 08:18:32 +0700

    Updated interface

--- src/Connect4.java
+++ src/Connect4.java
@@ -1,3 +1,5 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.geometry.Insets;
 import javafx.scene.Cursor;
 import javafx.scene.layout.AnchorPane;
@@ -7,6 +9,7 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 /**
  * Created by oj on 1/23/15.
@@ -61,7 +64,6 @@ public class Connect4 extends AnchorPane implements Connect4able {
         currentPlayer = Color.RED;
         highlightedColumn = new Rectangle(2 * (TOKEN_RADIUS + TOKEN_SPACING), ROWS * 2 * (TOKEN_RADIUS + TOKEN_SPACING));
 
-        highlightedColumn.setFill(currentPlayer);
         highlightedColumn.setOpacity(0.3);
 
         setMaxHeight(ROWS * 2 * (TOKEN_RADIUS + TOKEN_SPACING));
@@ -71,29 +73,6 @@ public class Connect4 extends AnchorPane implements Connect4able {
         setBackground(new Background(new BackgroundFill(Color.BLUE, new CornerRadii(TOKEN_RADIUS), new Insets(0))));
         setCursor(Cursor.CROSSHAIR);
 
-        setOnMouseClicked(event -> {
-            int col = (int) (event.getX() / (2 * (TOKEN_RADIUS + TOKEN_SPACING)));
-            System.out.println(col);
-        });
-
-        setOnMouseEntered(event -> {
-            if (!getChildren().contains(highlightedColumn)) {
-                getChildren().add(highlightedColumn);
-            }
-        });
-
-        setOnMouseExited(event -> {
-            if (getChildren().contains(highlightedColumn)) {
-                getChildren().remove(highlightedColumn);
-            }
-        });
-
-        setOnMouseMoved(event -> {
-            int col = (int) (event.getX() / (2 * (TOKEN_RADIUS + TOKEN_SPACING)));
-            highlightedColumn.setLayoutX(col * 2 * (TOKEN_RADIUS + TOKEN_SPACING));
-
-        });
-
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
                 Circle c = new Circle((TOKEN_RADIUS + TOKEN_SPACING) + j * 2 * (TOKEN_RADIUS + TOKEN_SPACING), (TOKEN_RADIUS + TOKEN_SPACING) + i * 2 * (TOKEN_RADIUS + TOKEN_SPACING), TOKEN_RADIUS, Color.WHITE);
@@ -101,6 +80,95 @@ public class Connect4 extends AnchorPane implements Connect4able {
                 getChildren().add(c);
             }
         }
+
+        processTurn();
+    }
+
+    private void processTurn() {
+        if (currentPlayer.equals(Color.RED) && redUI == null || currentPlayer.equals(Color.YELLOW) && yellowUI == null) {
+            highlightedColumn.setFill(currentPlayer);
+
+            setOnMouseClicked(event -> {
+                int col = (int) (event.getX() / (2 * (TOKEN_RADIUS + TOKEN_SPACING)));
+                addToken(col);
+                if (currentPlayer.equals(Color.RED))
+                    currentPlayer = Color.YELLOW;
+                else
+                    currentPlayer = Color.RED;
+                processTurn();
+            });
+
+            setOnMouseEntered(event -> {
+                if (!getChildren().contains(highlightedColumn)) {
+                    getChildren().add(highlightedColumn);
+                }
+            });
+
+            setOnMouseExited(event -> {
+                if (getChildren().contains(highlightedColumn)) {
+                    getChildren().remove(highlightedColumn);
+                }
+            });
+
+            setOnMouseMoved(event -> {
+                int col = (int) (event.getX() / (2 * (TOKEN_RADIUS + TOKEN_SPACING)));
+                highlightedColumn.setLayoutX(col * 2 * (TOKEN_RADIUS + TOKEN_SPACING));
+            });
+
+
+        } else if (currentPlayer.equals(Color.RED)) {
+            setOnMouseClicked(null);
+            setOnMouseEntered(null);
+            setOnMouseExited(null);
+            setOnMouseMoved(null);
+            Timeline timeline = new Timeline(new KeyFrame(new Duration(1000),event -> {
+                Color[][] board = duplicate();
+                addToken(redUI.getMove(board, Color.RED));
+                currentPlayer = Color.YELLOW;
+            }));
+            timeline.play();
+        } else {
+            setOnMouseClicked(null);
+            setOnMouseEntered(null);
+            setOnMouseExited(null);
+            setOnMouseMoved(null);
+            Timeline timeline = new Timeline(new KeyFrame(new Duration(1000),event -> {
+                Color[][] board = duplicate();
+                addToken(yellowUI.getMove(board, Color.YELLOW));
+                currentPlayer = Color.RED;
+            }));
+            timeline.play();
+        }
+
+    }
+
+    private void addToken(int col) {
+        int r = 0;
+
+        while (r < ROWS - 1 && (gameBoard[r][col].getFill()).equals(Color.WHITE)) {
+            r++;
+        }
+
+        if (r == 0 && (!(gameBoard[0][col].getFill()).equals(Color.WHITE))) {
+
+        } else {
+            if (!(gameBoard[r][col].getFill()).equals(Color.WHITE))
+               r--;
+
+            gameBoard[r][col].setFill(currentPlayer);
+        }
+    }
+
+    private Color[][] duplicate() {
+        Color[][] a = new Color[gameBoard.length][gameBoard[0].length];
+
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[0].length; j++) {
+                a[i][j] = (Color)gameBoard[i][j].getFill();
+            }
+        }
+
+        return a;
     }
 
 
